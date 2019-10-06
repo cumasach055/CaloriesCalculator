7be10dca718d8dcd8956cb673a9cb332d0b55175
arik93 <arik.li2509@gmail.com>
2019-10-06 20:32:38 +0600

    First Commit

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+let prices = [15000, 10000, 1000, 20000];
+let items = ["Table", "Chair", "Towel", "Mirror"];
+let i = 0
+let discount = 0.3 // 30% discount
 
 
+document.write ("<h1>", "Old Prices", "<br>", "</h1>")
+for (i = 0; i < prices.length; i++) {
+	document.write ("<li>", items[i] + " " + prices[i], "</li>", "<br>");
+}
 
-let hello = 'Здравствуйте!';
-let name =  'Аркадий';
-let surname = 'Ли';
-let language = 'JavaScript';
 
+document.write ("<h1>", "New Prices", "<br>", "</h1>")
+for (i = 0; i < prices.length; i++) {
+	document.write ("<li>", items[i] + " " + (prices[i] - (prices[i] * discount)), "</li>", "<br>");
+}
 
-alert(""+hello+" Меня зовут "+name+" "+surname+" и я изучаю "+language+"")
