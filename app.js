02216552840c2a8a833eb042f093ad675509089c
arik93 <arik.li2509@gmail.com>
2019-10-09 04:25:28 +0600

    First Commit

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-let prices = [15000, 10000, 1000, 20000];
-let items = ["Table", "Chair", "Towel", "Mirror"];
-let i = 0
-let discount = 0.3 // 30% discount
-
-
-document.write ("<h1>", "Old Prices", "<br>", "</h1>")
-for (i = 0; i < prices.length; i++) {
-	document.write ("<li>", items[i] + " " + prices[i], "</li>", "<br>");
+function calculateCalories() {
+	let currentWeight  = Number(prompt("Your current weight (kg)?"), "");
+	let desiredWeight = Number(prompt("Your desired weight (kg)?"), "");
+	let weeksCalculation = Number(prompt("How many weeks?"),"");
+	let firstResult = (((7700 * (currentWeight - desiredWeight)) / weeksCalculation) / 7);
+	let secondResult = (((7700 * (desiredWeight - currentWeight)) / weeksCalculation) / 7);
+	if (currentWeight > desiredWeight) {
+		alert("If you want to achive the desired wieght, you have to lose " + firstResult + " per day!");
+		let confirmation = confirm("Want to calculate calories again?");
+		if (confirmation === true) {
+			calculateCalories();
+		} 
+	} else {
+		alert("If you want to achive the desired wieght, you have to gain " + secondResult + " per day!");
+		let confirmation = confirm("Want to calculate calories again?");
+		if (confirmation === true) {
+			calculateCalories();
+		}
+	}
+	return firstResult;
+	return secondResult;
 }
 
 
-document.write ("<h1>", "New Prices", "<br>", "</h1>")
-for (i = 0; i < prices.length; i++) {
-	document.write ("<li>", items[i] + " " + (prices[i] - (prices[i] * discount)), "</li>", "<br>");
-}
+let calculation = calculateCalories();
+
+
+
+
+
 
